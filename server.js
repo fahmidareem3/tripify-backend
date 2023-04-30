db83cea202bc794a1fd5ab65df163b1df96ce5d4
fahmidareem3 <fahmidareem3@gmail.com>
2023-05-01 00:49:47 +0600

    feat: auth functionality created

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
+const auth = require("./routes/auth");
 const app = express();
 
 // Body parser
@@ -28,6 +29,7 @@ app.use(cors());
 
 app.use(errorHandler);
 
+app.use("/api/auth", auth);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
