7c767490dd843ab5c95432f1ef4c3ef95dbee6ef
fahmidareem3 <fahmidareem3@gmail.com>
2023-05-01 20:55:18 +0600

    fix : json data modified

--- controllers/flight.js
+++ controllers/flight.js
@@ -2,7 +2,7 @@ const asyncHandler = require("../middleware/async");
 const Flight = require("../models/Flights");
 
 // @desc      Create a flight
-// @route     POST /api/flights
+// @route     POST /api/flights/create
 // @access    Public
 exports.createFlight = asyncHandler(async (req, res, next) => {
   const {
